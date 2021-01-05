a8fa169b0f4b5175fa5c538210c478a2c902f5d8
dhanashri kokate <dhanashrikokate25@gmail.com>
2021-01-05 17:30:34 +0530

    initial commit

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,1 @@
+<h2>Welcome Laravel</h2>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <link rel="stylesheet" href={{ url('css/style.css') }} type="text/css" />
+    </head>
+    <body>
+        <h1> Welcome Page </h1>
+        {{-- <a href="/about">About</a>
+        <a href="/contact">contact</a> --}}
+    </body>
+</html>
+
+
+
